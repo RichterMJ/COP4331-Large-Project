6ccef56ece9ef57e7fc48bb5dbe3f09de3809be2
Santiago  Gutierrez <santigut@posteo.net>
2022-03-01 14:07:18 -0500

    Finished the standard header for the login page

--- food-parts/src/components/LoginPageHeader.js
+++ food-parts/src/components/LoginPageHeader.js
@@ -2,11 +2,16 @@ import React from "react";
 
 function LoginPageHeader() {
   return (
-    <div id ="LoginHeader">
-    <h1 className="headerText" id="title">
+    <div id ="LoginHeader" >
+      <Logo/>
+    <h1 className="left-text headerText" id="title">
       git fit
     </h1>
     </div>
   );
+}
+function Logo(){
+  const fileLocation = "./assets/thirdLogo.png"
+  return(<img id ="mainLogo" src ={fileLocation} alt="logo for gitfit"></img>)
 }
 export default LoginPageHeader;
