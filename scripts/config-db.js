e4ded3138b6efc25298c61bfb1ca9e72057bfc5a
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-03-14 13:08:41 -0400

    Fixed config-db.js

--- scripts/config-db.js
+++ scripts/config-db.js
@@ -1,15 +1,31 @@
 /* One-time script to configure the database. */
 
-import { MongoClient } from "mongodb";
-import 'dotenv/config'
+const mongodb = require('mongodb')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const dbUri = process.env.MONGODB_URI
-const dbClient = new MongoClient(dbUri)
+const dbClient = new mongodb.MongoClient(dbUri)
 
 ;(async () => {
-    await dbClient.connect()
 
-    dbClient.db().collection('Users').createIndex({ email: 1 }, { unique: true })
+    console.log('Configuring database...')
+
+    try {
+
+        await dbClient.connect()
+        await dbClient
+            .db()
+            .collection('Users')
+            .createIndex({ email: 1 }, { unique: true })
+
+    } catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
+
+    console.log('Done.')
+    process.exit()
 })()
 
 
