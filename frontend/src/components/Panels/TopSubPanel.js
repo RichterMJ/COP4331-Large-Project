b69e1b05b3137186c148b647be3d3578783e5a4d
Noah Seligson <noahseligson18@gmail.com>
2022-04-19 23:43:12 -0400

    Modified stuff to adjust to changes to TopSubPanel

--- frontend/src/components/Panels/TopSubPanel.js
+++ frontend/src/components/Panels/TopSubPanel.js
@@ -2,84 +2,131 @@ import React from "react";
 import {FoodSample} from "./PanelTestData";
 import {useState} from "react";
 import {makeActionButton} from "../divHelpers/divHelpers";
-import JSONRequest from "../RESTHelpers/JSONRequest";
-
+import {JSONRequest, JSONGETRequest} from "../RESTHelpers/JSONRequest";
 const storage = require("../tokenStorage.js");
 
-
 function TopSubPanel(props){
-    const [fl,setFl] = useState(props.foodList);
-    const [selectedFood, setSelectFood] = useState({});
-    const [displayedFoods, setDisplayedFoods] = useState(FoodSample);
-    
-    function makeRemoveFoodJSON(id){
-      const removeData = {
-        foodRecordId: id,
-        jwtToken: storage.retrieveToken()
-      }
-      return JSON.stringify(removeData);
-    }
-    
-    async function removeFood(index){
-      //For testing, this won't work anymore as it has already been deleted
-      //What this will do is get the foodrecordid of displayedFoods[index]
-      let temp = "625e0b4da53c56d46025d0d4";
-    
-      const deleteJSON = makeRemoveFoodJSON(temp);
-    
-      let res = await JSONRequest("DELETE", deleteJSON, "api/users/data/foodRecords");
-      //console.log(res.error);
 
-      FoodSample.splice(index,1);
-      setDisplayedFoods(FoodSample.slice(0));
+  function makeRemoveFoodJSON(id){
+    const removeData = {
+      foodRecordId: id,
+      jwtToken: storage.retrieveToken()
     }
+    return JSON.stringify(removeData);
+  }
+  
+  async function removeFood(index){
+    //For testing, this won't work anymore as it has already been deleted
+    //What this will do is get the foodrecordid of foods[index]
+    let temp = "625e0b4da53c56d46025d0d4";
+  
+    const deleteJSON = makeRemoveFoodJSON(temp);
+  
+    let res = await JSONRequest("DELETE", deleteJSON, "api/users/data/foodRecords");
     
-    function editFood(){}
-    
-    function makeFoodButtons(id, index){
-    
-        return(
-            <div className= "buttons">
-              {makeActionButton("button","removeFoodButton",() => removeFood(index),"x",id)}
-              {makeActionButton("button","removeFoodButton",() => editFood(index),"&#9998;",id)}
-            </div>
-        )
-    }
-    
-    function FoodElement(props){
-      let foodEl = props.food;
-      return(
-    
-        <div className = "dayFood" key={foodEl.id} >
-          <div className ="foodName">
-            {foodEl.name}
-          </div>
-          <div className ="foodAmount">
-            {foodEl.amount}
-          </div>
-          <div className ="foodUnit">
-            {foodEl.unit}
-          </div>
-          <div className ="foodCalories">
-            {foodEl.calories}
-          </div>
-          {makeFoodButtons(foodEl.id, props.index)}
-        </div>
-      )
-    }
-    
-    
-    function FoodList(props){
-      return(
-        props.foods.map((f, index) => <FoodElement key={f.id} food={f} index={index}/>)
-      )
-    }
+    console.log(res.error);
+  
+    foods.splice(index,1);
+    setDisplayedFoods(foods.slice(0));
+  }
+  
+  function editFood(){}
 
+  function makeFoodButtons(id, index){
+  
+      return(
+          <div className= "buttons">
+            {makeActionButton("button","removeFoodButton",() => removeFood(index),"x",id)}
+            {makeActionButton("button","removeFoodButton",() => editFood(index),"&#9998;",id)}
+          </div>
+      )
+  }
+  
+  function FoodElement(props){
+    let foodEl = props.food;
     return(
-      <div id = "topSubPanel">
-        <FoodList foods ={displayedFoods}/>
+  
+      <div className = "dayFood" key={foodEl.id} >
+        <div className ="foodName">
+          {foodEl.name}
+        </div>
+        <div className ="foodAmount">
+          {foodEl.amount}
+        </div>
+        <div className ="foodUnit">
+          {foodEl.unit}
+        </div>
+        <div className ="foodCalories">
+          {foodEl.calories}
+        </div>
+        {makeFoodButtons(foodEl.id, props.index)}
       </div>
     )
+  }
+  
+  
+  function FoodList(props){
+    return(
+      props.foods.map((f, index) => <FoodElement key={f.id} food={f} index={index}/>)
+    )
+  }
+
+  function makeFoodDayJSON(curDate){
+    const dateString = getDateString(curDate);
+    const foodReq = {
+      userId:props.userId,
+      startDate:dateString,
+      endDate:dateString,
+      jwtToken:storage.retrieveToken()
+    }
+    return foodReq;
+  }
+
+  function makeFoodDayURL(foodReq){
+    return "api/users/data/foodRecords/?userId="+foodReq.userId+"&startDate="+foodReq.startDate+"&endDate="+foodReq.endDate+"&jwtToken="+foodReq.jwtToken;
+  }
+
+  function handleFDGetRes(res){
+    if(res.error!=0){
+      console.log("error Happen"+res.error);
+      return [];
+    }
+    console.log(res.foodRecords);
+    return res.foodRecords
+  }
+
+  // this will get the latest version of the person's food day
+ async function getFoodDayList(startDate){
+    console.log(makeFoodDayURL(makeFoodDayJSON(startDate)));
+    let res = await JSONGETRequest(makeFoodDayURL(makeFoodDayJSON(startDate)));
+    return handleFDGetRes(res);
+  }
+  // this funtion adds leading zeros required by api
+  function prepMonth(month){
+    if(month.toString().length==1){
+      return "0"+month;
+    }
+    else{
+      return month;
+    }
+  }
+
+  function getDateString(date){
+    console.log(date);
+    return date.getFullYear() + "-" + prepMonth(date.getMonth())+ "-" + date.getDate();
+  }
+
+  const [fl,setFl] = useState(props.foodList);
+  let curDate = props.date;
+  let initialFoods = getFoodDayList(curDate)
+  const [foods,setFoods] = useState([]);
+  console.log(foods);
+
+  return(
+    <div id = "topSubPanel">
+      <FoodList foods ={foods}/>
+    </div>
+  )
 }
 
 export {TopSubPanel};
