363e4addbfedcd476f9f79904928cc54ec4c555c
Stephan Hartig <the.stephan.hartig@gmail.com>
2022-04-26 10:36:44 -0400

    Added missing RDI values

--- src/api/food/rdi-data.ts
+++ src/api/food/rdi-data.ts
@@ -1,7 +1,11 @@
 
-const TODO = -1
+const NON_ESSENTIAL = 0
 
-// Based on https://www.etoolsage.com/Chart/RDI_Chart.asp
+// Based on (in order of where I got them):
+// 1. https://www.etoolsage.com/Chart/RDI_Chart.asp
+// 2. https://www.myfooddata.com/articles/recommended-daily-intakes.php
+// 3. https://my.clevelandclinic.org/health/articles/22243-amino-acids
+// 
 export const RDI_CHART = [
     {
         "nutrientId": 1003,
@@ -25,31 +29,31 @@ export const RDI_CHART = [
         "nutrientId": 1009,
         "nutrientName": "Starch",
         "unitName": "G",
-        "value": TODO
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1010,
         "nutrientName": "Sucrose",
         "unitName": "G",
-        "value": TODO
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1011,
         "nutrientName": "Glucose (dextrose)",
         "unitName": "G",
-        "value": TODO
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1012,
         "nutrientName": "Fructose",
         "unitName": "G",
-        "value": TODO
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1013,
         "nutrientName": "Lactose",
         "unitName": "G",
-        "value": TODO
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1050,
@@ -106,10 +110,12 @@ export const RDI_CHART = [
         "value": 2400
     },
     {
+        // Uhhhh Sulfur just straight up has no RDI. This number is an estimate
+        // based on the RDI of sulfur containing amino acids.
         "nutrientId": 1094,
         "nutrientName": "Sulfur, S",
-        "unitName": "MG",
-        "value": TODO
+        "unitName": "UG",
+        "value": 13
     },
     {
         "nutrientId": 1095,
@@ -127,7 +133,7 @@ export const RDI_CHART = [
         "nutrientId": 1097,
         "nutrientName": "Cobalt, Co",
         "unitName": "UG",
-        "value": TODO
+        "value": 5
     },
     {
         "nutrientId": 1098,
@@ -138,8 +144,8 @@ export const RDI_CHART = [
     {
         "nutrientId": 1099,
         "nutrientName": "Fluoride, F",
-        "unitName": "UG",
-        "value": TODO
+        "unitName": "MG",
+        "value": 2.9
     },
     {
         "nutrientId": 1100,
@@ -257,7 +263,7 @@ export const RDI_CHART = [
     },
     {
         "nutrientId": 1178,
-        "nutrientName": "Vitamin BTODO2",
+        "nutrientName": "Vitamin B-12",
         "unitName": "UG",
         "value": 6
     },
@@ -294,116 +300,116 @@ export const RDI_CHART = [
     {
         "nutrientId": 1213,
         "nutrientName": "Leucine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 2730
     },
     {
         "nutrientId": 1214,
         "nutrientName": "Lysine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 2100
     },
     {
         "nutrientId": 1215,
         "nutrientName": "Methionine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 728
     },
     {
         "nutrientId": 1216,
         "nutrientName": "Cystine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 287
     },
     {
         "nutrientId": 1217,
         "nutrientName": "Phenylalanine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 875
     },
     {
         "nutrientId": 1218,
         "nutrientName": "Tyrosine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 875
     },
     {
         "nutrientId": 1219,
         "nutrientName": "Valine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 1820
     },
     {
         "nutrientId": 1220,
         "nutrientName": "Arginine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1221,
         "nutrientName": "Histidine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": 14
     },
     {
         "nutrientId": 1222,
         "nutrientName": "Alanine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1223,
         "nutrientName": "Aspartic acid",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1224,
         "nutrientName": "Glutamic acid",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1225,
         "nutrientName": "Glycine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1226,
         "nutrientName": "Proline",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1227,
         "nutrientName": "Serine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1232,
         "nutrientName": "Cysteine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1233,
         "nutrientName": "Glutamine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1234,
         "nutrientName": "Taurine",
-        "unitName": "G",
-        "value": TODO
+        "unitName": "MG",
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1257,
         "nutrientName": "Fatty acids, total trans",
         "unitName": "G",
-        "value": TODO
+        "value": NON_ESSENTIAL
     },
     {
         "nutrientId": 1258,
@@ -415,19 +421,19 @@ export const RDI_CHART = [
         "nutrientId": 1291,
         "nutrientName": "Fatty acids, other than 607-615, 617-621, 624-632, 652-654, 686-689)",
         "unitName": "G",
-        "value": TODO
+        "value": 4
     },
     {
         "nutrientId": 1292,
         "nutrientName": "Fatty acids, total monounsaturated",
         "unitName": "G",
-        "value": TODO
+        "value": 36
     },
     {
         "nutrientId": 1293,
         "nutrientName": "Fatty acids, total polyunsaturated",
         "unitName": "G",
-        "value": TODO
+        "value": 9
     },
     {
         "nutrientId": 2047,
