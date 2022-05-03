8ddbc4435f85aebd74f99d2c00c36bfbb05e0192
Hieu Dang <dangh2809@gmail.com>
2022-05-03 16:06:38 -0400

    fix responseModal size

--- frontend/src/components/Modals/ResponseModal.js
+++ frontend/src/components/Modals/ResponseModal.js
@@ -6,20 +6,20 @@ const ResoponseModal = ({ setIsOpen, responseMessage }) => {
     <>
       <div className="darkBG" />
       <div className="centered">
-        <div className="verifyModal">
+        <div className="responseModal">
           <div className="modalContent">
             {responseMessage}!
           </div>
-          <div className="modalActions">
-            <div className="actionsContainer">
-              <button
-                className="okayBtn"
-                onClick={() => (window.location.href = "/")}
-              >
-                Okay
-              </button>
+            <div className="modalActions">
+              <div className="actionsContainer">
+                <button
+                  className="okayBtn"
+                  onClick={() => (window.location.href = "/")}
+                >
+                  Okay
+                </button>
+              </div>
             </div>
-          </div>
         </div>
       </div>
     </>
